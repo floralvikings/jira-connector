c82ca9bfbf50b72a4c212db54aac926786ee50c5
Caleb Brinkman <Caleb.Brinkman@whitehatsec.com>
2014-12-30 17:34:50 -0600

    Added utility method for generating oauth authorization URLs.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 "use strict";
 
+// npm packages
+var Oauth = require('oauth');
+
+// Core packages
+var url = require('url');
+
+var NO_HOST_ERROR = 'Missing \'host\' property.';
 var NO_CONSUMER_KEY_ERROR = 'Missing \'oauth_consumer_key\' property.';
 var NO_PRIVATE_KEY_ERROR = 'Missing \'oauth_private_key\' property.';
 var NO_OAUTH_TOKEN_ERROR = 'Missing \'oauth_token\' property.';
@@ -64,3 +71,71 @@ var Client = module.exports = function(config) {
         throw new Error(INVALID_AUTHENTICATION_PROPERTY_ERROR);
     }
 }
+
+/**
+ * Attempts to get an OAuth verification URL using the given API configuration.
+ *
+ * @param {Object} config The information needed to access the Jira API
+ * @param {string} config.host The hostname of the Jira API.
+ * @param {string} [config.protocol=https] - The protocol used to accses the Jira API.
+ * @param {number} [config.port=443] - The port number used to connect to Jira.
+ * @param {string} [config.version=2] - The version of the Jira API to which you will be connecting.  Currently, only version 2 is supported.
+ * @param {Object} config.oauth The oauth information
+ * @param {string} config.oauth.consumer_key The consumer key of the application accessing Jira.
+ * @param {string} config.oauth.private_key The private key of the application accessing Jira.
+ * @param {string} [config.oauth.callback_url] The callback URL to be called after the token is generated.  If this is not
+ *        included, the user will be given a verification code after authorizing the token, instead of Jira making a
+ *        callback to the application.
+ * @param {getOauthUrlCallback} callback The function called when the URL has been retrieved.
+ */
+exports.getAuthorizeURL = function(config, callback) {
+    var SERVLET_BASE_URL = '/plugins/servlet';
+    var REQ_TOKEN_APPEND = '/oauth/request-token';
+    var AUTH_TOKEN_APPEND = '/oauth/authorize';
+    var sig = 'RSA-SHA1';
+
+    if(!config.host) {
+        throw new Error(NO_HOST_ERROR);
+    }else if(!config.oauth.consumer_key) {
+        throw new Error(NO_CONSUMER_KEY_ERROR);
+    } else if(!config.oauth.private_key) {
+        throw new Error(NO_PRIVATE_KEY_ERROR);
+    }
+
+    var consumer_key = config.oauth.consumer_key;
+    var private_key = config.oauth.private_key;
+
+    var reqURL = url.format( {
+        protocol: config.protocol ? config.protocol : 'https',
+        hostname: config.host,
+        port: config.port ? config.port : null,
+        pathname: SERVLET_BASE_URL + REQ_TOKEN_APPEND
+    });
+
+    var authURL = url.format({
+        protocol: config.protocol ? config.protocol : 'https',
+        hostname: config.host,
+        port: config.port ? config.port : 443,
+        pathname: SERVLET_BASE_URL + AUTH_TOKEN_APPEND
+    });
+
+    console.log(reqURL);
+
+    var cb = config.oauth.callback_url ? config.oauth.callback_url : 'oob';
+
+    var oauth = new Oauth.OAuth(reqURL, authURL, consumer_key, private_key, '1.0', cb, sig);
+
+    oauth.getOAuthRequestToken(function(err, token, token_secret) {
+        if(err) {
+            return callback(err);
+        }
+        return callback(null, {url: authURL + "?oauth_token=" + token, token: token, token_secret: token_secret});
+    });
+};
+
+/**
+ * Callback used by getOauthUrl.
+ * @callback getOauthUrlCallback
+ * @param {string} error The error which occurred, if any.
+ * @param {string} url The OAuth verification URL retrieved from the Jira API.
+ */
