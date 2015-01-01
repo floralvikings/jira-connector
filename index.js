316a7eb7b7046f72cb3c85af4832f75df83486db
Caleb Brinkman <floralvikings@gmail.com>
2014-12-31 20:37:50 -0600

    Extracted function to generate OAuth object, added function to swap OAuth request token for access token.

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ var NO_OAUTH_TOKEN_SECRET_ERROR = 'Missing \'oauth_token_secret\' property.';
 var NO_USERNAME_ERROR = 'Missing \'username\' property.';
 var NO_PASSWORD_ERROR = 'Missing \'password\' property.';
 var NO_AUTHENTICATION_ERROR = 'Missing \'auth\' property.';
+var NO_VERIFIER_ERROR = 'Missing \'oauth_verifier\' property.';
 var INVALID_AUTHENTICATION_PROPERTY_ERROR = 'Invalid \'auth\' property.';
 
 /**
@@ -90,9 +91,69 @@ var Client = module.exports = function (config) {
  * @param {getOauthUrlCallback} callback The function called when the URL has been retrieved.
  */
 exports.getAuthorizeURL = function (config, callback) {
+    var oauth = generateOAuthObject(config);
+
+    oauth.getOAuthRequestToken(function (err, token, token_secret) {
+        if (err) {
+            return callback(err);
+        }
+        return callback(null, {url: authURL + "?oauth_token=" + token, token: token, token_secret: token_secret});
+    });
+};
+
+/**
+ * Given an OAuth token, the token secret, and an access verification code (provided by Jira), swap an OAuth request
+ * token with an OAuth access token.
+ *
+ * @param {Object} config The information needed to access the Jira API
+ * @param {string} config.host The hostname of the Jira API.
+ * @param {string} [config.protocol=https] - The protocol used to accses the Jira API.
+ * @param {number} [config.port=443] - The port number used to connect to Jira.
+ * @param {string} [config.version=2] - The version of the Jira API to which you will be connecting.  Currently, only version 2 is supported.
+ * @param {Object} config.oauth The oauth information
+ * @param {string} config.oauth.consumer_key The consumer key of the application accessing Jira.
+ * @param {string} config.oauth.private_key The private key of the application accessing Jira.
+ * @param {string} config.oauth.oauth_token The OAuth Token supplied by Jira.
+ * @param {string} config.oauth.oauth_token_secret The OAuth Token secret supplied by Jira.
+ * @param {string} config.oauth.oauth_verifier The verified code given to the user after authorizing the OAuth token.
+ * @param {swapRequestTokenCallback} callback The function called when the token has been swapped.
+ */
+exports.swapRequestTokenWithAccessToken = function(config, callback) {
+    if(!config.oauth.oauth_verifier) {
+        throw new Error(NO_VERIFIER_ERROR);
+    }
+
+    var oauth = generateOAuthObject(config);
+
+    var token = config.oauth.oauth_token;
+    var secret = config.oauth.oauth_token_secret;
+    var verifier = config.oauth.oauth_verifier;
+
+    oauth.getOAuthAccessToken(token, secret, verifier, callback);
+};
+
+/**
+ * Utility function to generate an OAuth object.
+ *
+ * @param {Object} config The information needed to access the Jira API
+ * @param {string} config.host The hostname of the Jira API.
+ * @param {string} [config.protocol=https] - The protocol used to accses the Jira API.
+ * @param {number} [config.port=443] - The port number used to connect to Jira.
+ * @param {string} [config.version=2] - The version of the Jira API to which you will be connecting.  Currently, only version 2 is supported.
+ * @param {Object} config.oauth The oauth information
+ * @param {string} config.oauth.consumer_key The consumer key of the application accessing Jira.
+ * @param {string} config.oauth.private_key The private key of the application accessing Jira.
+ * @param {string} [config.oauth.callback_url] The callback URL to be called after the token is generated.  If this is not
+ *        included, the user will be given a verification code after authorizing the token, instead of Jira making a
+ *        callback to the application.
+ *
+ * @returns {exports.OAuth} The generated object.
+ */
+function generateOAuthObject(config) {
     var SERVLET_BASE_URL = '/plugins/servlet';
     var REQ_TOKEN_APPEND = '/oauth/request-token';
     var AUTH_TOKEN_APPEND = '/oauth/authorize';
+    var ACCESS_TOKEN_APPEND = '/oauth/access-token';
     var sig = 'RSA-SHA1';
 
     if (!config.host) {
@@ -116,30 +177,37 @@ exports.getAuthorizeURL = function (config, callback) {
     var authURL = url.format({
         protocol: config.protocol ? config.protocol : 'https',
         hostname: config.host,
-        port: config.port ? config.port : 443,
+        port: config.port ? config.port : null,
         pathname: SERVLET_BASE_URL + AUTH_TOKEN_APPEND
+    });
+
+    var accessURL = url.format({
+        protocol: config.protocol ? config.protocol : 'https',
+        hostname: config.host,
+        port: config.port ? config.port : null,
+        pathname: SERVLET_BASE_URL + ACCESS_TOKEN_APPEND
     });
 
     console.log(reqURL);
 
     var cb = config.oauth.callback_url ? config.oauth.callback_url : 'oob';
 
-    var oauth = new Oauth.OAuth(reqURL, authURL, consumer_key, private_key, '1.0', cb, sig);
-
-    oauth.getOAuthRequestToken(function (err, token, token_secret) {
-        if (err) {
-            return callback(err);
-        }
-        return callback(null, {url: authURL + "?oauth_token=" + token, token: token, token_secret: token_secret});
-    });
-};
+    return new Oauth.OAuth(reqURL, accessURL, consumer_key, private_key, '1.0', cb, sig);
+}
 
 /**
  * Callback used by getOauthUrl.
  * @callback getOauthUrlCallback
- * @param {string} error The error which occurred, if any.
+ * @param {*} error The error which occurred, if any.
  * @param {Object} oauth The OAuth information retrieved from the Jira API.
  * @param {String} oauth.url The URL that should be visited by the user to verify the OAuth access.
  * @param {String} oauth.token The OAuth Token retrieved from the Jira API.
  * @param {String} oauth.token_secret The OAuth Token Secret retrieved from the Jira API.
+ */
+
+/**
+ * Callback used by swapRequestTokenWithAccessToken
+ * @callback swapRequestTokenCallback
+ * @param {*} error The error which occurred, if any.
+ * @param {string} access_token The access token retrieved from Jira.
  */
