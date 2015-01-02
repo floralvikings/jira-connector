ee3b1a42a9cd26218183da8df947e9bf77fe8720
Caleb Brinkman <floralvikings@gmail.com>
2015-01-02 15:24:23 -0600

    Added a large amount of documentation

--- api/issue.js
+++ api/issue.js
@@ -8,7 +8,82 @@ var issue = module.exports = function(jiraClient) {
 
     /**
      * Create a new issue in the Jira tracker.
-     * @param {Object} issue
+     * @param {Object} issue The issue data in the form of POST body to the JIRA API.  Example:
+     * {
+     * "update": {
+     *     "worklog": [
+     *         {
+     *             "add": {
+     *                 "started": "2011-07-05T11:05:00.000+0000",
+     *                 "timeSpent": "60m"
+     *             }
+     *         }
+     *     ]
+     *  },
+     *  "fields": {
+     *      "project": {
+     *        "id": "10000"
+     *    },
+     *    "summary": "something's wrong",
+     *    "issuetype": {
+     *        "id": "10000"
+     *    },
+     *    "assignee": {
+     *        "name": "homer"
+     *    },
+     *    "reporter": {
+     *        "name": "smithers"
+     *    },
+     *    "priority": {
+     *        "id": "20000"
+     *    },
+     *    "labels": [
+     *        "bugfix",
+     *        "blitz_test"
+     *    ],
+     *    "timetracking": {
+     *        "originalEstimate": "10",
+     *        "remainingEstimate": "5"
+     *    },
+     *    "security": {
+     *        "id": "10000"
+     *    },
+     *    "versions": [
+     *        {
+     *            "id": "10000"
+     *        }
+     *    ],
+     *    "environment": "environment",
+     *    "description": "description",
+     *    "duedate": "2011-03-11",
+     *    "fixVersions": [
+     *        {
+     *            "id": "10001"
+     *        }
+     *    ],
+     *    "components": [
+     *        {
+     *            "id": "10000"
+     *        }
+     *    ],
+     *    "customfield_60000": "jira-developers",
+     *    "customfield_20000": "06/Jul/11 3:25 PM",
+     *    "customfield_80000": {
+     *        "value": "red"
+     *    },
+     *    "customfield_40000": "this is a text field",
+     *    "customfield_30000": [
+     *        "10000",
+     *        "10002"
+     *    ],
+     *    "customfield_70000": [
+     *        "jira-administrators",
+     *        "jira-users"
+     *    ],
+     *    "customfield_50000": "this is a text area. big text.",
+     *    "customfield_10000": "09/Jun/81"
+     *  }
+     * }
      * @param callback Called when the issue has been created.
      */
     this.create = function (issue, callback) {
