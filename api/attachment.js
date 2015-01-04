49f25c25e279d92eb2757faf236fe42610997ad9
Caleb Brinkman <floralvikings@gmail.com>
2015-01-04 02:00:38 -0600

    Added boilerplate code for AttachmentClient class

---
+++ api/attachment.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = AttachmentClient;
+
+/**
+ * Used to access Jira REST endpoints in '/rest/api/2/atachment'
+ *
+ * @constructor AttachmentClient
+ * @param {JiraClient} jiraClient
+ */
+function AttachmentClient(jiraClient) {
+
+}
