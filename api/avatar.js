c1aa3a87fcdd840330921d1733032f62664ceb22
Caleb Brinkman <floralvikings@gmail.com>
2015-01-05 15:40:04 -0600

    Added boilerplate AvatarClient class.

---
+++ api/avatar.js
@@ -0,0 +1,32 @@
+"use strict";
+
+module.exports = AvatarClient;
+
+/**
+ * Used to access Jira REST endpoints in '/rest/api/2/issue'
+ * @param {JiraClient} jiraClient
+ * @constructor AvatarClient
+ */
+function AvatarClient(jiraClient) {
+    this.jiraClient = jiraClient;
+
+    /**
+     * Helper method to reduce duplicated code.  Uses the JiraClient to make a request, calling back with either
+     * the response, or the supplied error string if it exists.
+     *
+     * @method makeRequest
+     * @memberOf IssueClient#
+     * @param {Object} options The requiest options
+     * @param {Function} callback Called with the Jira APIs response.
+     * @param {string} [successString] If supplied, this is reported instead of the response body.
+     */
+    this.makeRequest = function (options, callback, successString) {
+        this.jiraClient.makeRequest(options, function (err, response, body) {
+            if (err || response.statusCode.toString()[0] != 2) {
+                return callback(err ? err : body);
+            }
+
+            return callback(null, successString ? successString : body);
+        });
+    };
+}
