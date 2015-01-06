29e5648ad8a0d25af120ea1b8d6f7af15d6714f5
Caleb Brinkman <floralvikings@gmail.com>
2015-01-05 22:14:42 -0600

    Added createIssueLink method, tested and working

--- api/issueLink.js
+++ api/issueLink.js
@@ -1,9 +1,43 @@
 "use strict";
 
+var errorStrings = require('./../lib/error');
+
 module.exports = IssueLinkClient;
 
 function IssueLinkClient(jiraClient) {
     this.jiraClient = jiraClient;
+
+    /**
+     * Creates an issue link between two issues. The user requires the link issue permission for the issue which will
+     * be linked to another issue. The specified link type in the request is used to create the link and will create a
+     * link from the first issue to the second issue using the outward description. It also create a link from the
+     * second issue to the first issue using the inward description of the issue link type. It will add the supplied
+     * comment to the first issue. The comment can have a restriction who can view it. If group is specified, only
+     * users of this group can view this comment, if roleLevel is specified only users who have the specified role can
+     * view this comment. The user who creates the issue link needs to belong to the specified group or have the
+     * specified role.
+     *
+     * @memberOf IssueLinkClient#
+     * @method createIssueLink
+     * @param opts The options for the request sent to the Jira API
+     * @param opts.issueLink See {@link https://docs.atlassian.com/jira/REST/latest/#d2e5010}
+     * @param callback Called when the link has been created.
+     */
+    this.createIssueLink = function (opts, callback) {
+        if (!opts.issueLink) {
+            throw new Error(errorStrings.NO_ISSUE_LINK_ERROR);
+        }
+
+        var options = {
+            method: 'POST',
+            uri: this.jiraClient.buildURL('/issueLink'),
+            json: true,
+            followAllRedirects: true,
+            body: opts.issueLink
+        };
+
+        this.makeRequest(options, callback, 'Issue Link Created');
+    };
 
     /**
      * Helper method to reduce duplicated code.  Uses the JiraClient to make a request, calling back with either
