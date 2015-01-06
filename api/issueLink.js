de65182fe7747716657f805ce7282faddc4bd414
Caleb Brinkman <floralvikings@gmail.com>
2015-01-05 21:57:39 -0600

    Added boilerplate IssueLinkClient class

---
+++ api/issueLink.js
@@ -0,0 +1,27 @@
+"use strict";
+
+module.exports = IssueLinkClient;
+
+function IssueLinkClient(jiraClient) {
+    this.jiraClient = jiraClient;
+
+    /**
+     * Helper method to reduce duplicated code.  Uses the JiraClient to make a request, calling back with either
+     * the response, or the supplied error string if it exists.
+     *
+     * @method makeRequest
+     * @memberOf IssueLinkClient#
+     * @param {Object} options The request options
+     * @param {Function} callback Called with the Jira APIs response.
+     * @param {string} [successString] If supplied, this is reported instead of the response body.
+     */
+    this.makeRequest = function (options, callback, successString) {
+        this.jiraClient.makeRequest(options, function (err, response, body) {
+            if (err || response.statusCode.toString()[0] != 2) {
+                return callback(err ? err : body);
+            }
+
+            return callback(null, successString ? successString : body);
+        });
+    };
+}
