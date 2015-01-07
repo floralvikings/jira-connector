c9051ef502d7f8c39249ef06e6b91b063f86ef9f
Caleb Brinkman <floralvikings@gmail.com>
2015-01-06 23:02:05 -0600

    Added getCustomFieldOption method, tested and working.

--- api/customFieldOption.js
+++ api/customFieldOption.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var errorStrings = require('./../lib/error');
+
 module.exports = CustomFieldOptionClient;
 
 /**
@@ -10,4 +12,27 @@ module.exports = CustomFieldOptionClient;
  */
 function CustomFieldOptionClient(jiraClient) {
     this.jiraClient = jiraClient;
+
+    /**
+     * Returns a full representation of the Custom Field Option that has the given id.
+     *
+     * @method getCustomFieldOption
+     * @param {Object} opts The request options sent to the Jira API
+     * @param {string} opts.fieldOptionId A String containing an Custom Field Option id
+     * @param callback
+     */
+    this.getCustomFieldOption = function (opts, callback) {
+        if (!opts.fieldOptionId) {
+            throw new Error(errorStrings.NO_FIELD_OPTION_ID_ERROR);
+        }
+
+        var options = {
+            uri: this.jiraClient.buildURL('/customFieldOption/' + opts.fieldOptionId),
+            method: 'GET',
+            json: true,
+            followAllRedirects: true
+        };
+
+        this.jiraClient.makeRequest(options, callback);
+    };
 }
