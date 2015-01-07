1ae2376ae4f14251929b0717c7cd688dc236cf22
Caleb Brinkman <floralvikings@gmail.com>
2015-01-07 16:28:18 -0600

    Added getSecurityLevel method, tested and working.

--- api/securityLevel.js
+++ api/securityLevel.js
@@ -10,4 +10,24 @@ module.exports = SecurityLevelClient;
  */
 function SecurityLevelClient(jiraClient) {
     this.jiraClient = jiraClient;
+
+    /**
+     * Get a full representation of the security level that has the given id.
+     *
+     * @method getSecurityLevel
+     * @memberOf SecurityLevelClient#
+     * @param opts The request options to send to the Jira API.
+     * @param opts.securityLevelId The ID of the security level to retrieve
+     * @param callback Called when the security level has been retrieved.
+     */
+    this.getSecurityLevel = function (opts, callback) {
+        var options = {
+            uri: this.jiraClient.buildURL('/securitylevel/' + opts.securityLevelId),
+            method: 'GET',
+            json: true,
+            followAllRedirects: true
+        };
+
+        this.jiraClient.makeRequest(options, callback);
+    }
 }
