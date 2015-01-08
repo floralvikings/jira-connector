31571c606c1e13f0c8517d4f391f19d526d07c0b
Caleb Brinkman <floralvikings@gmail.com>
2015-01-08 17:09:18 -0600

    Added getPasswordPolicy method, tested and working.

--- api/password.js
+++ api/password.js
@@ -10,4 +10,29 @@ module.exports = PasswordClient;
  */
 function PasswordClient(jiraClient) {
     this.jiraClient = jiraClient;
+
+    /**
+     * Returns user-friendly statements governing the system's password policy.
+     *
+     * @method getPasswordPolicy
+     * @memberOf {PasswordClient#}
+     * @param opts The request options to send to the Jira API
+     * @param {boolean} [opts.hasOldPassword=false] Whether or not the user will be required to enter their current
+     *     password. Use false (the default) if this is a new user or if an administrator is forcibly changing another
+     *     user's password.
+     * @param callback Called when the password policy has been retrieved.
+     */
+    this.getPasswordPolicy = function (opts, callback) {
+        var options = {
+            uri: this.jiraClient.buildURL('/password/policy'),
+            method: 'GET',
+            json: true,
+            followAllRedirects: true,
+            qs: {
+                hasOldPassword: opts.hasOldPassword
+            }
+        };
+
+        this.jiraClient.makeRequest(options, callback);
+    }
 }
