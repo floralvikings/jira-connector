0aef1cf524de779d91734258df6ffd919cb719c7
Caleb Brinkman <floralvikings@gmail.com>
2015-01-08 17:55:52 -0600

    Added editComponent method, tested and working.

--- api/component.js
+++ api/component.js
@@ -31,4 +31,29 @@ function ComponentClient(jiraClient) {
 
         this.jiraClient.makeRequest(options, callback);
     };
+
+    /**
+     * Modify a component via PUT. Any fields present in the PUT will override existing values. As a convenience, if a
+     * field is not present, it is silently ignored. If leadUserName is an empty string ("") the component lead will be
+     * removed.
+     *
+     * @method editComponent
+     * @memberOf {ComponentClient#}
+     * @param opts The request options sent to the Jira API.
+     * @param opts.id The ID of the component to edit.
+     * @param opts.component The new data to place in the component.  See
+     *      {@link https://docs.atlassian.com/jira/REST/latest/#d2e3939}
+     * @param callback Called when the component has beed edited.
+     */
+    this.editComponent = function (opts, callback) {
+        var options = {
+            uri: this.jiraClient.buildURL('/component/' + opts.id),
+            method: 'PUT',
+            json: true,
+            followAllRedirects: true,
+            body: opts.component
+        };
+
+        this.jiraClient.makeRequest(options, callback);
+    };
 }
