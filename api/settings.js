6e717bcbf319549154169e15857af18d139cacaf
Caleb Brinkman <floralvikings@gmail.com>
2015-01-08 17:20:11 -0600

    Added setBaseUrl function, tested and working.

--- api/settings.js
+++ api/settings.js
@@ -9,4 +9,25 @@ module.exports = SettingsClient;
  */
 function SettingsClient(jiraClient) {
     this.jiraClient = jiraClient;
+
+    /**
+     * Sets the base URL that is configured for this JIRA instance.
+     *
+     * @method setBaseUrl
+     * @memberOf {SettingsClient#}
+     * @param opts The request options sent to the Jira API.
+     * @param opts.newUrl The new base url.
+     * @param callback Called when the base url has been set.
+     */
+    this.setBaseUrl = function (opts, callback) {
+        var options = {
+            uri: this.jiraClient.buildURL('/settings/baseUrl'),
+            method: 'PUT',
+            json: true,
+            followAllRedirects: true,
+            body: opts.newUrl
+        };
+
+        this.jiraClient.makeRequest(options, callback);
+    }
 }
