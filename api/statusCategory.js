dd51163ecc4483a7d28ac3b4ddda2fef9cc32967
Caleb Brinkman <floralvikings@gmail.com>
2015-01-08 12:34:46 -0600

    Added getAllStatusCategories method, tested and working

--- api/statusCategory.js
+++ api/statusCategory.js
@@ -10,4 +10,23 @@ module.exports = StatusCategoryClient;
  */
 function StatusCategoryClient(jiraClient) {
     this.jiraClient = jiraClient;
+
+    /**
+     * Returns a list of all statusCategories visible to the user
+     *
+     * @method getAllStatusCategories
+     * @memberOf StatusCategoryClient#
+     * @param opts Ignored
+     * @param callback Called when the statusCategories have been retrieved.
+     */
+    this.getAllStatusCategories = function (opts, callback) {
+        var options = {
+            uri: this.jiraClient.buildURL('/statuscategory'),
+            method: 'GET',
+            json: true,
+            followAllRedirects: true
+        };
+
+        this.jiraClient.makeRequest(options, callback);
+    };
 }
