d7d5eec437a1fd42619ea47105d16ba3db224ebe
Caleb Brinkman <floralvikings@gmail.com>
2015-01-08 16:08:30 -0600

    getPreference method added, tested and working

--- api/myPreferences.js
+++ api/myPreferences.js
@@ -10,4 +10,28 @@ module.exports = MyPreferencesClient;
  */
 function MyPreferencesClient(jiraClient) {
     this.jiraClient = jiraClient;
+
+    /**
+     * Gets preference of the currently logged in user. Preference key must be provided as input parameter (key).
+     * The value is returned exactly as it is.
+     *
+     * @method getPreferences
+     * @memberOf {MyPreferencesClient#}
+     * @param opts The request options send to the Jira API.
+     * @param opts.key Key of the preference to be returned.
+     * @param callback Called when the preference has been retrieved.
+     */
+    this.getPreference = function (opts, callback) {
+        var options = {
+            uri: this.jiraClient.buildURL('/mypreferences'),
+            method: 'GET',
+            json: true,
+            followAllRedirects: true,
+            qs: {
+                key: opts.key
+            }
+        };
+
+        this.jiraClient.makeRequest(options, callback);
+    }
 }
