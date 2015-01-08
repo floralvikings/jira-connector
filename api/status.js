e974c4295790deae75a8322fd7a007ee9373647f
Caleb Brinkman <floralvikings@gmail.com>
2015-01-08 12:12:42 -0600

    Added getAllStatuses method, tested and working.

--- api/status.js
+++ api/status.js
@@ -10,4 +10,23 @@ module.exports = StatusClient;
  */
 function StatusClient(jiraClient) {
     this.jiraClient = jiraClient;
+
+    /**
+     * Returns a list of all statuss visible to the user
+     *
+     * @method getAllStatuses
+     * @memberOf StatusClient#
+     * @param opts Ignored
+     * @param callback Called when the statuss have been retrieved.
+     */
+    this.getAllStatuses = function (opts, callback) {
+        var options = {
+            uri: this.jiraClient.buildURL('/status'),
+            method: 'GET',
+            json: true,
+            followAllRedirects: true
+        };
+
+        this.jiraClient.makeRequest(options, callback);
+    };
 }
