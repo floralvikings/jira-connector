7f3dc2e591c854c93c5d5e6cf785844cd486fca3
Caleb Brinkman <floralvikings@gmail.com>
2015-01-08 10:27:25 -0600

    Added getAllResolutions method, tested and working.

--- api/resolution.js
+++ api/resolution.js
@@ -10,4 +10,23 @@ module.exports = ResolutionClient;
  */
 function ResolutionClient(jiraClient) {
     this.jiraClient = jiraClient;
+
+    /**
+     * Returns a list of all resolutions visible to the user
+     *
+     * @method getAllResolutions
+     * @memberOf ResolutionClient#
+     * @param opts Ignored
+     * @param callback Called when the resolutions have been retrieved.
+     */
+    this.getAllResolutions = function (opts, callback) {
+        var options = {
+            uri: this.jiraClient.buildURL('/resolution'),
+            method: 'GET',
+            json: true,
+            followAllRedirects: true
+        };
+
+        this.jiraClient.makeRequest(options, callback);
+    };
 }
