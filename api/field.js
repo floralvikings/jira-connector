764a7371130242676d2a2d9d90713943692b76a9
Caleb Brinkman <floralvikings@gmail.com>
2015-01-07 18:07:47 -0600

    Added createCustomField method, tested and working.

--- api/field.js
+++ api/field.js
@@ -10,4 +10,25 @@ module.exports = FieldClient;
  */
 function FieldClient(jiraClient) {
     this.jiraClient = jiraClient;
+
+    /**
+     * Creates a custom field using a definition (object encapsulating custom field data)
+     *
+     * @method createCustomField
+     * @memberOf FieldClient#
+     * @param opts The request options to send to Jira
+     * @param opts.field See {@link https://docs.atlassian.com/jira/REST/latest/#d2e3412}
+     * @param callback Called when the custom field has been created.
+     */
+    this.createCustomField = function (opts, callback) {
+        var options = {
+            uri: this.jiraClient.buildURL('/field'),
+            method: 'POST',
+            json: true,
+            followAllRedirects: true,
+            body: opts.field
+        };
+
+        this.jiraClient.makeRequest(options, callback);
+    }
 }
