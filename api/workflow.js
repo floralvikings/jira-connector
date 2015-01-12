040eb3d77a5c8cf231013fb8dacf7212d72b39d8
Caleb Brinkman <floralvikings@gmail.com>
2015-01-12 12:26:43 -0600

    Added getWorkflows method, tested and working.

--- api/workflow.js
+++ api/workflow.js
@@ -10,4 +10,25 @@ module.exports = WorkflowClient;
  */
 function WorkflowClient(jiraClient) {
     this.jiraClient = jiraClient;
+
+    /**
+     * Returns all workflows.
+     *
+     * @param {Object} opts The request options sent to the Jira API
+     * @param {string} [opts.workflowName] The name of the workflow to retrieve.
+     * @param callback Called when the workflow(s) have been retrieved.
+     */
+    this.getWorkflows = function (opts, callback) {
+        var options = {
+            uri: this.jiraClient.buildURL('/workflow'),
+            method: 'GET',
+            json: true,
+            followAllRedirects: true,
+            qs: {
+                workflowName: opts.workflowName
+            }
+        };
+
+        this.jiraClient.makeRequest(options, callback);
+    };
 }
