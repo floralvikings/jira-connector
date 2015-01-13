3d0a1cbc7538b192bc963be40285d22b13ade7c2
Caleb Brinkman <floralvikings@gmail.com>
2015-01-13 13:42:14 -0600

    Added empty getAvailableFields method

--- api/screen.js
+++ api/screen.js
@@ -10,4 +10,17 @@ module.exports = ScreenClient;
  */
 function ScreenClient(jiraClient) {
     this.jiraClient = jiraClient;
+
+    /**
+     * Gets available fields for screen. i.e ones that haven't already been added.
+     *
+     * @method getAvailableFields
+     * @memberOf {ScreenClient#}
+     * @param {Object} opts The request options sent to Jira
+     * @param {number} opts.screenId The ID of the screen to retrieve.
+     * @param callback Called when the available fields have been retrieved
+     */
+    this.getAvailableFields = function (opts, callback) {
+
+    }
 }
