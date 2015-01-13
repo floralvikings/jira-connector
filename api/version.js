bc2e3a8fb0ec9fc3df411b7f0cde075572fabd2b
Caleb Brinkman <floralvikings@gmail.com>
2015-01-13 16:41:51 -0600

    Added buildRequestOptions method

--- api/version.js
+++ api/version.js
@@ -30,4 +30,51 @@ function VersionClient(jiraClient) {
 
         this.jiraClient.makeRequest(options, callback);
     };
+
+    /**
+     * Build out the request options necessary to make a particular API call.
+     *
+     * @private
+     * @method buildRequestOptions
+     * @memberOf FilterClient#
+     * @param {Object} opts The arguments passed to the method.
+     * @param {number} opts.versionId The ID of the screen to use in the path.
+     * @param {Array} [opts.fields] The fields to include
+     * @param {Array} [opts.expand] The fields to expand
+     * @param {string} path The path of the endpoint following /version/{id}
+     * @param {string} method The request method.
+     * @param {Object} [body] The request body, if any.
+     * @param {Object} [qs] The querystring, if any.  opts.expand and opts.fields arrays will be automagically added.
+     * @returns {{uri: string, method: string, body: Object, qs: Object, followAllRedirects: boolean, json: boolean}}
+     */
+    this.buildRequestOptions = function (opts, path, method, body, qs) {
+        var basePath = '/version/' + opts.versionId;
+        if (!qs) qs = {};
+        if (!body) body = {};
+
+        if (opts.fields) {
+            qs.fields = '';
+            opts.fields.forEach(function (field) {
+                qs.fields += field + ','
+            });
+            qs.fields = qs.fields.slice(0, -1);
+        }
+
+        if (opts.expand) {
+            qs.expand = '';
+            opts.expand.forEach(function (ex) {
+                qs.expand += ex + ','
+            });
+            qs.expand = qs.expand.slice(0, -1);
+        }
+
+        return {
+            uri: this.jiraClient.buildURL(basePath + path),
+            method: method,
+            body: body,
+            qs: qs,
+            followAllRedirects: true,
+            json: true
+        };
+    };
 }
