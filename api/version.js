feed6ed7301ba79f9325e697178cd6f4fc3d4706
Caleb Brinkman <floralvikings@gmail.com>
2015-01-13 16:38:38 -0600

    Added createVersion method, tested and working.

--- api/version.js
+++ api/version.js
@@ -9,4 +9,25 @@ module.exports = VersionClient;
  */
 function VersionClient(jiraClient) {
     this.jiraClient = jiraClient;
+
+    /**
+     * Creates a version
+     *
+     * @method createVersion
+     * @memberOf VersionClient#
+     * @param {Object} opts The request options sent to Jira.
+     * @param {Object} opts.version See {@link https://docs.atlassian.com/jira/REST/latest/#d2e3549}
+     * @param callback Called when the version has been created.
+     */
+    this.createVersion = function (opts, callback) {
+        var options = {
+            uri: this.jiraClient.buildURL('/version'),
+            method: 'POST',
+            json: true,
+            followAllRedirects: true,
+            body: opts.version
+        };
+
+        this.jiraClient.makeRequest(options, callback);
+    };
 }
