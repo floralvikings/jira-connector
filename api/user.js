41b7977ceff3b5e1482983f45527bc22222a3f86
Caleb Brinkman <floralvikings@gmail.com>
2015-01-14 16:01:33 -0600

    Added getUser method, tested and working.

--- api/user.js
+++ api/user.js
@@ -10,4 +10,29 @@ module.exports = UserClient;
  */
 function UserClient(jiraClient) {
     this.jiraClient = jiraClient;
+
+    /**
+     * Get a user. This resource cannot be accessed anonymously.
+     *
+     * @method getUser
+     * @memberOf UserClient#
+     * @param opts The request options sent to the Jira API
+     * @param opts.username The name of the user to retrieve.
+     * @param opts.userKey The key of the user to retrieve.
+     * @param callback Called when the user has been retrieved.
+     */
+    this.getUser = function (opts, callback) {
+        var options = {
+            uri: this.jiraClient.buildURL('/user'),
+            method: 'GET',
+            json: true,
+            followAllRedirects: true,
+            qs: {
+                username: opts.username,
+                key: opts.userKey
+            }
+        };
+
+        this.jiraClient.makeRequest(options, callback);
+    };
 }
