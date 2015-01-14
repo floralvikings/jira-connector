1eb868689ca18cbe757863d574c7a11026bcb404
Caleb Brinkman <floralvikings@gmail.com>
2015-01-13 18:21:18 -0600

    Added buildRequestOptions method

--- api/project.js
+++ api/project.js
@@ -9,4 +9,51 @@ module.exports = ProjectClient;
  */
 function ProjectClient(jiraClient) {
     this.jiraClient = jiraClient;
+
+    /**
+     * Build out the request options necessary to make a particular API call.
+     *
+     * @private
+     * @method buildRequestOptions
+     * @memberOf ProjectClient#
+     * @param {Object} opts The arguments passed to the method.
+     * @param {number} opts.projectId The ID of the project to use in the path.
+     * @param {Array} [opts.fields] The fields to include
+     * @param {Array} [opts.expand] The fields to expand
+     * @param {string} path The path of the endpoint following /project/{id}
+     * @param {string} method The request method.
+     * @param {Object} [body] The request body, if any.
+     * @param {Object} [qs] The querystring, if any.  opts.expand and opts.fields arrays will be automagically added.
+     * @returns {{uri: string, method: string, body: Object, qs: Object, followAllRedirects: boolean, json: boolean}}
+     */
+    this.buildRequestOptions = function (opts, path, method, body, qs) {
+        var basePath = '/project/' + opts.projectId;
+        if (!qs) qs = {};
+        if (!body) body = {};
+
+        if (opts.fields) {
+            qs.fields = '';
+            opts.fields.forEach(function (field) {
+                qs.fields += field + ','
+            });
+            qs.fields = qs.fields.slice(0, -1);
+        }
+
+        if (opts.expand) {
+            qs.expand = '';
+            opts.expand.forEach(function (ex) {
+                qs.expand += ex + ','
+            });
+            qs.expand = qs.expand.slice(0, -1);
+        }
+
+        return {
+            uri: this.jiraClient.buildURL(basePath + path),
+            method: method,
+            body: body,
+            qs: qs,
+            followAllRedirects: true,
+            json: true
+        };
+    };
 }
