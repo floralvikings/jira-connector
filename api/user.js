4e5c7e57f7eec264b884d86e7ee5850005274aeb
Caleb Brinkman <floralvikings@gmail.com>
2015-01-15 16:36:05 -0600

    Added searchPermissions method, tested and working.

--- api/user.js
+++ api/user.js
@@ -398,4 +398,52 @@ function UserClient(jiraClient) {
         };
         this.jiraClient.makeRequest(options, callback, 'Password Changed');
     };
+
+    /**
+     * Returns a list of active users that match the search string and have all specified permissions for the project
+     * or issue.
+     *
+     * This resource can be accessed by users with ADMINISTER_PROJECT permission for the project or global
+     * ADMIN or SYSADMIN rights.
+     *
+     * @method searchPermissions
+     * @memberOf UserClient#
+     * @param {Object} opts The request options sent to the jira API
+     * @param {string} opts.username The username filter, list includes all users if unspecified
+     * @param {Array} opts.permissions Array of permissions for project or issue returned users must have, see
+     *     [Permissions]{@link
+        *     https://developer.atlassian.com/static/javadoc/jira/6.0/reference/com/atlassian/jira/security/Permissions.Permission.html}
+        *     JavaDoc for the list of all possible permissions.
+     * @param {string} [opts.issueKey] the issue key for the issue for which returned users have specified permissions.
+     * @param {string} [opts.projectKey] the optional project key to search for users with if no issueKey is supplied.
+     * @param {number} [opts.startAt] the index of the first user to return (0-based)
+     * @param {number} [opts.maxResults] the maximum number of users to return (defaults to 50). The maximum allowed
+     *     value is 1000. If you specify a value that is higher than this number, your search results will be
+     *     truncated.
+     * @param callback Called when the search results are retrieved.
+     */
+    this.searchPermissions = function (opts, callback) {
+        var permissions = '';
+        if (opts.permissions) {
+            opts.permissions.forEach(function (s) {
+                permissions += s + ','
+            });
+            permissions = permissions.slice(0, -1);
+        }
+        var options = {
+            uri: this.jiraClient.buildURL('/user/permission/search'),
+            method: 'GET',
+            json: true,
+            followAllRedirects: true,
+            qs: {
+                username: opts.username,
+                permissions: permissions,
+                issueKey: opts.issueKey,
+                projectKey: opts.projectKey,
+                startAt: opts.startAt,
+                maxResults: opts.maxResults
+            }
+        };
+        this.jiraClient.makeRequest(options, callback);
+    }
 }
