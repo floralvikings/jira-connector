806eeabfc61ab37065a9561463c3e7718360aa85
Caleb Brinkman <floralvikings@gmail.com>
2015-01-15 17:33:23 -0600

    Added createWorkflowScheme method, tested and working.

--- api/workflowScheme.js
+++ api/workflowScheme.js
@@ -9,4 +9,25 @@ module.exports = WorkflowSchemeClient;
  */
 function WorkflowSchemeClient(jiraClient) {
     this.jiraClient = jiraClient;
+
+    /**
+     * Create a new workflow scheme. The body contains a representation of the new scheme. Values not passed are
+     * assumed to be set to their defaults.
+     *
+     * @method createWorkflowScheme
+     * @memberOf WorkflowSchemeClient#
+     * @param opts The request options sent to the Jira API.
+     * @param opts.workflowScheme See {@link https://docs.atlassian.com/jira/REST/latest/#d2e2196}
+     * @param callback Called when the workflow scheme has been created.
+     */
+    this.createWorkflowScheme = function (opts, callback) {
+        var options = {
+            uri: this.jiraClient.buildURL('/workflowscheme'),
+            method: 'POST',
+            json: true,
+            followAllRedirects: true,
+            body: opts.workflowScheme
+        };
+        this.jiraClient.makeRequest(options, callback);
+    };
 }
