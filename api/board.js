42b31c3dc1635d18e325704cbd8b50bf4eb5ab74
Will Olson <willko747@gmail.com>
2017-01-08 15:52:07 -0600

    adding getSprintsForBoard to api/board

--- api/board.js
+++ api/board.js
@@ -101,4 +101,36 @@ function AgileBoardClient(jiraClient) {
 
       return this.jiraClient.makeRequest(options, callback);
   };
+
+  /**
+   * Get a list of all issues associated with an agile board
+   *
+   * @method getSprintsForBoard
+   * @memberOf AgileBoardClient#
+   * @param opts The request options to send to the Jira API
+   * @param opts.boardId The agile board id.
+   * @param [opts.startAt] The index of the first dashboard to return (0-based). must be 0 or a multiple of
+   *     maxResults
+   * @param [opts.maxResults] A hint as to the the maximum number of dashboards to return in each call. Note that the
+   *     JIRA server reserves the right to impose a maxResults limit that is lower than the value that a client
+   *     provides, dues to lack or resources or any other condition. When this happens, your results will be
+   *     truncated. Callers should always check the returned maxResults to determine the value that is effectively
+   *     being used.
+   * @param callback Called when the dashboards have been retrieved.
+   * @return {Promise} Resolved when the dashboards have been retrieved.
+   */
+  this.getSprintsForBoard = function (opts, callback) {
+    var options = {
+      uri: this.jiraClient.buildAgileURL('/board/' + opts.boardId + '/sprint'),
+      method: 'GET',
+      json: true,
+      followAllRedirects: true,
+      qs: {
+        startAt: opts.startAt,
+        maxResults: opts.maxResults
+      }
+    };
+
+    return this.jiraClient.makeRequest(options, callback);
+  };
 }
