1f7bf1f949e54e8a272ae724da927d35a22a94aa
Ishtiaque Zafar <ishtiaque.zafar92@gmail.com>
2018-02-06 19:19:07 +0530

    added permission-scheme

--- api/permissions.js
+++ api/permissions.js
@@ -9,26 +9,26 @@ module.exports = PermissionsClient;
  * @constructor PermissionsClient
  */
 function PermissionsClient(jiraClient) {
-  this.jiraClient = jiraClient;
+    this.jiraClient = jiraClient;
 
-  /**
-   * Returns all permissions that are present in the JIRA instance
-   * - Global, Project and the global ones added by plugins
-   *
-   * @method getAllPermissions
-   * @memberOf PermissionsClient#
-   * @param opts The request options sent to the Jira API.
-   * @param [callback] Called when the permissions have been returned.
-   * @return {Promise} Resolved when the permissions have been returned.
-   */
-  this.getAllPermissions = function (opts, callback) {
-    var options = {
-      uri: this.jiraClient.buildURL('/permissions'),
-      method: 'GET',
-      json: true,
-      followAllRedirects: true
-    };
+    /**
+     * Returns all permissions that are present in the JIRA instance
+     * - Global, Project and the global ones added by plugins
+     *
+     * @method getAllPermissions
+     * @memberOf PermissionsClient#
+     * @param opts The request options sent to the Jira API.
+     * @param [callback] Called when the permissions have been returned.
+     * @return {Promise} Resolved when the permissions have been returned.
+     */
+    this.getAllPermissions = function (opts, callback) {
+        var options = {
+            uri: this.jiraClient.buildURL('/permissions'),
+            method: 'GET',
+            json: true,
+            followAllRedirects: true
+        };
 
-    return this.jiraClient.makeRequest(options, callback);
-  }
+        return this.jiraClient.makeRequest(options, callback);
+    }
 }
