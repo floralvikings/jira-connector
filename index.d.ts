7096eef8247acfa699e948a05c71ee6e4672969e
Vladislav Tupikin <MrRefactoring@yandex.ru>
2019-06-08 23:04:10 +0300

    Basic types definitions added (#152)
    * basic types definitions added
    
    * export changed

---
+++ index.d.ts
@@ -0,0 +1,99 @@
+import { Epic } from './api/epic.d.ts';
+
+export = JiraClient;
+
+interface IConfig {
+    host: string;
+    port?: number;
+
+    protocol?: string;
+    path_prefix?: string;
+    strictSSL?: boolean;
+    version?: string;
+    basic_auth?: {
+        username?: string;
+        password?: string;
+        base64?: string;
+    };
+    oauth?: {
+        consumer_key: string;
+        private_key: string;
+        token: string;
+        token_secret: string;
+    }
+    cookie_jar?: any;
+    promise?: PromiseLike<any>;
+    request?: any;
+    rejectUnauthorized?: any;
+}
+
+declare class JiraClient {
+    constructor(config: IConfig);
+
+    public applicationProperties: any;
+    public attachment: any;
+    public auditing: any;
+    public auth: any;
+    public avatar: any;
+    public backlog: any;
+    public board: any;
+    public comment: any;
+    public component: any;
+    public customFieldOption: any;
+    public dashboard: any;
+    public epic: Epic;
+    public field: any;
+    public filter: any;
+    public group: any;
+    public groupUserPicker: any;
+    public groups: any;
+    public issue: any;
+    public issueLink: any;
+    public issueLinkType: any;
+    public issueType: any;
+    public jql: any;
+    public labels: any;
+    public licenseRole: any;
+    public licenseValidator: any;
+    public myPermissions: any;
+    public myPreferences: any;
+    public myself: any;
+    public password: any;
+    public permissions: any;
+    public permissionScheme: any;
+    public priority: any;
+    public project: any;
+    public projectCategory: any;
+    public projectValidate: any;
+    public reindex: any;
+    public resolution: any;
+    public roles: any;
+    public screens: any;
+    public search: any;
+    public securityLevel: any;
+    public serverInfo: any;
+    public settings: any;
+    public sprint: any;
+    public status: any;
+    public statusCategory: any;
+    public user: any;
+    public version: any;
+    public webhook: any;
+    public workflow: any;
+    public workflowScheme: any;
+    public worklog: any;
+
+    public buildAgileURL(path: string, forcedVersion?: number | string): string;
+    public buildAuthURL(path: string, forcedVersion?: number | string): string;
+    public buildURL(path: string, forcedVersion?: number | string): string;
+    public buildWebhookURL(path: string, forcedVersion?: number | string): string;
+    public makeRequest(options: { [key: string]: any }, callback?: any, successString?: string): any;
+}
+
+declare namespace JiraClient {
+    namespace oauth_util {
+        function getAuthorizeURL(config: any, callback: any): any;
+
+        function swapRequestTokenWithAccessToken(config: any, callback: any): void;
+    }
+}
